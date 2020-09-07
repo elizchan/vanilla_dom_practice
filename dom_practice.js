1bd07724adc7e70e4629f8d9f631924d0bb7ac6e
elizchan <lizchan@bu.edu>
2020-09-07 00:03:09 -0400

    created a variable to get elements by class

---
+++ dom_practice.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const sub = document.getElementById("sub")
+    console.log(sub)
+    const h2 = document.querySelector("h2")
+    console.log(h2)
+    h2.innerText="Welcome to Edison!!!"
+    const list = document.getElementsByClassName("list")
+    console.log(list)
+})
